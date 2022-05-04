842b83058727a46e53173f47932d0962c3108b93
Padilla-UW <77411509+Padilla-UW@users.noreply.github.com>
2022-05-03 21:30:58 -0500

    carrusel inferior

--- peliculas/lib/widgets/movie_slider.dart
+++ peliculas/lib/widgets/movie_slider.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MovieSlider extends StatelessWidget {
@@ -7,8 +8,7 @@ class MovieSlider extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      height: 250,
-      color: Colors.red,
+      height: 300,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -18,6 +18,9 @@ class MovieSlider extends StatelessWidget {
               'Populares',
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
+          ),
+          SizedBox(
+            height: 5,
           ),
           Expanded(
             child: ListView.builder(
@@ -38,9 +41,33 @@ class _MoviePoster extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 130,
-      height: 190,
-      color: Colors.green,
+      height: 200,
       margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Column(children: [
+        GestureDetector(
+          onTap: () => Navigator.pushNamed(context, 'details',
+              arguments: 'movie-instance'),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/300x400'),
+              width: 130,
+              height: 190,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        const Text(
+          'starwars: el retorno en el cerro de montecristo',
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.center,
+        )
+      ]),
     );
   }
 }
